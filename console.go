d03940c85439df8da218e4f8f392672f9b1e9733
xwi88 <xwi_88@163.com>
2018-12-23 17:38:43 +0800

    add console writer

---
+++ console.go
@@ -0,0 +1,133 @@
+package log4go
+
+import (
+	"fmt"
+	"os"
+)
+
+type colorRecord Record
+
+// brush is a color join function
+type brush func(string) string
+
+// newBrush return a fix color Brush
+func newBrush(color string) brush {
+	pre := "\033["
+	reset := "\033[0m"
+	return func(text string) string {
+		return fmt.Sprintf("%s%s%s%s%s", pre, color, "m", text, reset)
+	}
+}
+
+var colors = []brush{
+	newBrush("1;37"), // Emergency          white
+	newBrush("1;36"), // Alert              cyan
+	newBrush("1;35"), // Critical           magenta
+	newBrush("1;31"), // Error              red
+	newBrush("1;33"), // Warning            yellow
+	newBrush("1;32"), // Notice             green
+	newBrush("1;34"), // Informational      blue
+	newBrush("1;44"), // Debug              Background blue
+}
+
+func (r *colorRecord) ColorString() string {
+	inf := fmt.Sprintf("%s %s %s %s\n", r.time, LevelFlags[r.level], r.file, r.msg)
+	return colors[r.level](inf)
+}
+
+func (r *colorRecord) String() string {
+	inf := ""
+	switch r.level {
+	case EMERGENCY:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[37m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case ALERT:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[36m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case CRITICAL:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[35m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case ERROR:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[31m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case WARNING:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[33m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case NOTICE:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[32m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case INFO:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[34m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	case DEBUG:
+		inf = fmt.Sprintf("\033[36m%s\033[0m [\033[44m%s\033[0m] \033[47;30m%s\033[0m %s\n",
+			r.time, LevelFlags[r.level], r.file, r.msg)
+	}
+
+	return inf
+}
+
+// ConsoleWriter console writer define
+type ConsoleWriter struct {
+	level     int
+	color     bool
+	fullColor bool // line all with color
+}
+
+// ConsoleWriterOptions color field options
+type ConsoleWriterOptions struct {
+	Level     int
+	Color     bool
+	FullColor bool
+}
+
+// NewConsoleWriter create new console writer
+func NewConsoleWriter() *ConsoleWriter {
+	return &ConsoleWriter{}
+}
+
+// NewConsoleWriterWithOptions create new console writer with level
+func NewConsoleWriterWithOptions(option ConsoleWriterOptions) *ConsoleWriter {
+	defaultLevel := DEBUG
+
+	if option.Level <= defaultLevel {
+		defaultLevel = option.Level
+	}
+	return &ConsoleWriter{
+		level:     defaultLevel,
+		color:     option.Color,
+		fullColor: option.FullColor,
+	}
+}
+
+// Write console write
+func (w *ConsoleWriter) Write(r *Record) error {
+	if r.level > w.level {
+		return nil
+	}
+	if w.color {
+		if w.fullColor {
+			fmt.Fprint(os.Stdout, ((*colorRecord)(r)).ColorString())
+		} else {
+			fmt.Fprint(os.Stdout, ((*colorRecord)(r)).String())
+		}
+	} else {
+		fmt.Fprint(os.Stdout, r.String())
+	}
+	return nil
+}
+
+// Init console init without implement
+func (w *ConsoleWriter) Init() error {
+	return nil
+}
+
+// SetColor console output color control
+func (w *ConsoleWriter) SetColor(c bool) {
+	w.color = c
+}
+
+// SetFullColor console output full line color control
+func (w *ConsoleWriter) SetFullColor(c bool) {
+	w.fullColor = c
+}
---
+++ console_test.go
@@ -0,0 +1,303 @@
+package log4go
+
+import (
+	"testing"
+)
+
+func generateNewConsoleWriterWithOptions(level int, color, fullColor bool) *ConsoleWriter {
+	options := ConsoleWriterOptions{
+		Level:     level,
+		Color:     color,
+		FullColor: fullColor,
+	}
+	w := NewConsoleWriterWithOptions(options)
+	w.SetColor(color)
+	w.SetFullColor(fullColor)
+	return w
+}
+
+func generateRegisterConsoleWriter(lg *Logger, w *ConsoleWriter, fullPath, funcName bool, layout string) {
+	lg.Register(w)
+	if layout == "" {
+		lg.SetLayout("2006-01-02 15:04:05")
+	} else {
+		lg.SetLayout(layout)
+	}
+	lg.WithFullPath(fullPath)
+	lg.WithFuncName(funcName)
+}
+
+func Test_NewConsoleWriterWithStruct(t *testing.T) {
+	c := &ConsoleWriter{}
+	t.Log(c)
+}
+
+func Test_NewConsoleWriter(t *testing.T) {
+	NewConsoleWriter()
+}
+
+func Test_NewConsoleWriterWithNilLogger(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+	records := make(chan *Record, uint(0))
+	close(records)
+	loggerDefaultTest := newLoggerWithRecords(records)
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console nil logger"
+	defer func() {
+		if err := recover(); err != nil {
+			t.Errorf("error occur: %v", err)
+			loggerDefaultTest = newLoggerWithRecords(records)
+			generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+			defer loggerDefaultTest.Close()
+			loggerDefaultTest.Debug("log4go by %s", name)
+			loggerDefaultTest.Info("log4go by %s", name)
+			loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+		}
+	}()
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+
+}
+
+func Test_NewConsoleWriterWithGlobalSet(t *testing.T) {
+	var color, fullColor bool
+	var layout string
+	loggerDefault = NewLogger()
+
+	defer Close()
+	layout = "20060102 150304"
+	SetLayout(layout)
+	SetLevel(INFO)
+	WithFullPath(true)
+	WithFuncName(true)
+	c := generateNewConsoleWriterWithOptions(INFO, color, fullColor)
+	Register(c)
+
+	var name = "console with default global"
+	Debug("log4go by %s", name)
+	Info("log4go by %s", name)
+	Info("")
+	Notice("log4go by %s", name)
+	Warn("log4go by %s", name)
+	Error("log4go by %s", name)
+	Critical("log4go by %s", name)
+	Alert("log4go by %s", name)
+	Emergency("log4go by %s", name)
+}
+
+func Test_NewConsoleWriterWithLevel(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	loggerDefaultTest.SetLevel(DEBUG)
+	defer loggerDefaultTest.Close()
+
+	c := generateNewConsoleWriterWithOptions(INFO, color, fullColor)
+	var name = "console level"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Info("")
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithLevel2(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	loggerDefaultTest.SetLevel(NOTICE)
+	defer loggerDefaultTest.Close()
+
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console level2"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithColor(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	defer loggerDefaultTest.Close()
+
+	color = true
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console color"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithFullColor(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	defer loggerDefaultTest.Close()
+
+	color = true
+	fullColor = true
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console full color"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithFullPath(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	defer loggerDefaultTest.Close()
+
+	color = true
+	fullPath = true
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console full path"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithFuncName(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	defer loggerDefaultTest.Close()
+
+	color = true
+	funcName = true
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console func name"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Test_NewConsoleWriterWithLayout(t *testing.T) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	defer loggerDefaultTest.Close()
+
+	color = true
+	layout = "20060102150304000"
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console layout"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Benchmark_NewConsoleWriter(b *testing.B) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	loggerDefaultTest.SetLevel(DEBUG)
+	defer loggerDefaultTest.Close()
+
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console benchmark test"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
+
+func Benchmark_NewConsoleWriterAll(b *testing.B) {
+	var color, fullColor, fullPath, funcName bool
+	var layout string
+
+	records := make(chan *Record, uint(2048))
+	loggerDefaultTest := newLoggerWithRecords(records)
+	loggerDefaultTest.SetLevel(DEBUG)
+	defer loggerDefaultTest.Close()
+	color = true
+	fullColor = true
+	fullPath = true
+	funcName = true
+	layout = "2006-01-02 15:04:05"
+
+	c := generateNewConsoleWriterWithOptions(DEBUG, color, fullColor)
+	var name = "console benchmark test"
+	generateRegisterConsoleWriter(loggerDefaultTest, c, fullPath, funcName, layout)
+	loggerDefaultTest.Debug("log4go by %s", name)
+	loggerDefaultTest.Info("log4go by %s", name)
+	loggerDefaultTest.Notice("log4go by %s", name)
+	loggerDefaultTest.Warn("log4go by %s", name)
+	loggerDefaultTest.Error("log4go by %s", name)
+	loggerDefaultTest.Critical("log4go by %s", name)
+	loggerDefaultTest.Alert("log4go by %s", name)
+	loggerDefaultTest.Emergency("log4go by %s", name)
+	loggerDefaultTest.Alert("%#v", loggerDefaultTest)
+}
